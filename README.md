cf7c91fa83c4d3267d63503c1ae0152a1e1ab8e6
Naveen Santhosh Prabhakar <naveesanthosh.prof@gmail.com>
2023-10-15 22:55:07 +0100

    Commit_01 15Oct

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ToDoApp
+pip freeze > requirements.txt 
---
+++ main.py
@@ -0,0 +1,11 @@
+import dataconn
+import display
+
+
+
+def main(string):
+    print ("HELLO {}!!".format(string))
+
+
+if __name__ == "__main__" :
+    main(string="random")
